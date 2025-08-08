7361ac4efaeeb67e507e6c0960426e0265ce72b0
unknown <fasionergg@gmail.com>
2025-08-08 23:31:39 +0300

    Мой первый коммит

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-// next.config.js или next.config.mjs
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  // Добавьте другие необходимые настройки
-  images: {
-    unoptimized: true, // Обязательно для статического экспорта
-  },
-  trailingSlash: true, // Рекомендуется для статических экспортов
-  distDir: "out", // Опционально - каталог для экспорта
+   output: "export",
 };
 
 export default nextConfig;
